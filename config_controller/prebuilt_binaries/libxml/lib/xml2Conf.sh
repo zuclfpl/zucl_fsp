27bd6e641261b6099261fded6afd3bbfab3142b5
Khoa Pham <khoa002@e.ntu.edu.sg>
2018-07-16 10:23:32 +0100

    Prebuilt binaries for libxml2 on ARM 64-bit CPU

---
+++ config_controller/prebuilt_binaries/libxml/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/guillermo/libxml/lib"
+XML2_LIBS="-lxml2      -lm "
+XML2_INCLUDEDIR="-I/home/guillermo/libxml/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
